83e7e28dc892f5aaafa0573fdba78da9fab47976
Christian <ch.grimm@gmx.net>
2024-09-18 20:21:49 +0200

    colorscheme nordic.

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -13,7 +13,7 @@ return {
       vim.g.nord_bold = false
 
       -- Load the colorscheme
-      require('nord').set()
+      -- require('nord').set()
 
       -- Toggle background transparency
       local bg_transparent = true
@@ -42,6 +42,61 @@ return {
       --
       --   -- You can configure highlights by doing something like:
       --   vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('nordic').setup {
+        -- This callback can be used to override the colors used in the palette.
+        on_palette = function(palette)
+          return palette
+        end,
+        -- Enable bold keywords.
+        bold_keywords = false,
+        -- Enable italic comments.
+        italic_comments = false,
+        -- Enable general editor background transparency.
+        transparent_bg = false,
+        -- Enable brighter float border.
+        bright_border = false,
+        -- Reduce the overall amount of blue in the theme (diverges from base Nord).
+        reduced_blue = true,
+        -- Swap the dark background with the normal one.
+        swap_backgrounds = false,
+        -- Override the styling of any highlight group.
+        override = {},
+        -- Cursorline options.  Also includes visual/selection.
+        cursorline = {
+          -- Bold font in cursorline.
+          bold = true,
+          -- Bold cursorline number.
+          bold_number = true,
+          -- Available styles: 'dark', 'light'.
+          theme = 'dark',
+          -- Blending the cursorline bg with the buffer bg.
+          blend = 0.15,
+        },
+        noice = {
+          -- Available styles: `classic`, `flat`.
+          style = 'classic',
+        },
+        telescope = {
+          -- Available styles: `classic`, `flat`.
+          style = 'flat',
+        },
+        leap = {
+          -- Dims the backdrop when using leap.
+          dim_backdrop = false,
+        },
+        ts_context = {
+          -- Enables dark background for treesitter-context window
+          dark_background = true,
+        },
+      }
+      require('nordic').load()
     end,
   },
 }
