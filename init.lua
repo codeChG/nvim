fc42924c151f8e51c4f8e216d7162a0365afcf85
Christian <ch.grimm@gmx.net>
2024-09-21 21:52:16 +0200

    standard colorscheme gesetzt (nordic).

--- init.lua
+++ init.lua
@@ -52,6 +52,6 @@ require('lazy').setup {
   -- Dynamisch alle Colorschemes laden
   unpack(load_colorschemes()),
 }
-
+vim.cmd 'colorscheme nordic'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
