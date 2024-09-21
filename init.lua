0361ceea6f708412077676c5d812f7f49d535810
Christian <ch.grimm@gmx.net>
2024-09-21 19:53:23 +0200

    netrw aktiviert, neotree deaktiviert.

--- init.lua
+++ init.lua
@@ -2,10 +2,23 @@ require 'core.options' -- Load general options
 require 'core.keymaps' -- Load general keymaps
 require 'core.snippets' -- Custom code snippets
 
--- Deaktiviert alle netrw-Funktionen
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- Funktion, um alle Colorschemes aus dem Ordner zu laden
+local function load_colorschemes()
+  local colorschemes_path = vim.fn.stdpath 'config' .. '/lua/plugins/colorschemes/'
+  local colorschemes = {}
 
+  -- Alle Lua-Dateien im Colorschemes-Ordner finden
+  for _, file in ipairs(vim.fn.glob(colorschemes_path .. '*.lua', 0, 1)) do
+    -- Extrahiere den Dateinamen ohne .lua und Ordner
+    local scheme_name = file:match '.*/(.*)%.lua$'
+    if scheme_name then
+      -- Farben hinzufügen
+      table.insert(colorschemes, { import = 'plugins.colorschemes.' .. scheme_name })
+    end
+  end
+
+  return colorschemes
+end
 -- Set up the Lazy plugin manager
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -20,7 +33,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Set up plugins
 require('lazy').setup {
   require 'plugins.colorschemes.nordic',
-  require 'plugins.neotree',
+  -- require 'plugins.neotree',
   require 'plugins.bufferline',
   require 'plugins.lualine',
   require 'plugins.treesitter',
@@ -36,6 +49,8 @@ require('lazy').setup {
   require 'plugins.nvim-lint',
   require 'plugins.rainbow-delimiters',
   require 'plugins.toogleterm',
+  -- Dynamisch alle Colorschemes laden
+  unpack(load_colorschemes()),
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
